5ced95a334f0c658e612c915cf72c6cfee3cfdbb
Yigit Er <mr.yigiter222@gmail.com>
2023-08-03 07:52:22 +0300

    practice

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import Adres from "./components/component1/Adres";
+import Bders from "./components/component1/Bders";
+import Cders from "./components/component1/Cders";
+
+import Practice2 from "./components/component2/practice2";
+import Practice3 from "./components/component3/practice3";
+import Tablo from "./components/component4/tablo";
+import Table from "./components/component5/table";
+import Kisiler from "./components/component6/kisiler";
+import Profiles from "./components/component7/profiles";
+import Cards from "./components/component8/cards";
+import StudentCard from "./components/component8/studentCard";
+import { Reviews } from "./components/component9/reviews";
+
+import Akis from "./components/component10/Akis";
+import Counter from "./components/component11/counter";
+import RandomImage from "./components/component11/randomImage";
+import Bilgiler from "./components/component12/bilgiler";
+import Listele from "./components/component13/listele";
+import Text from "./components/component14/text";
+import UserCards from "./components/component16/user-cards";
+import Countries from "./components/component15/countries";
+import Form1 from "./components/component17/form";
+import Tours from "./components/component18/tours";
+import Questions from "./components/component19/questions";
+
+
+function App() {
+  return (
+    <>
+      {/*       <Adres></Adres>
+      <Bders></Bders>
+      <Cders></Cders> 
+      <Practice2></Practice2>
+      <Practice3></Practice3>
+       <Tablo></Tablo>
+       <Table></Table>
+       <Kisiler></Kisiler>
+       <Profiles></Profiles>
+       <Cards></Cards>
+       <Reviews></Reviews>
+       <Akis></Akis>
+       <Reviews></Reviews>
+      <Counter></Counter>
+      <RandomImage></RandomImage>
+      <Bilgiler/>
+      <Listele/>
+      <Text/>
+       <UserCards></UserCards>
+ <Countries></Countries>
+  <Form1/>
+    <Tours></Tours>
+      */}
+     
+ 
+   <Questions></Questions>
+      
+      
+    </>
+  );
+}
+
+export default App;
